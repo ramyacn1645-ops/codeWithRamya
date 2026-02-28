0abf4c70f75c3b04396c02368a878cb5ae40d79a
Ramya C N <ramyacn1645@gmaul.com>
2026-02-28 23:44:38 +0530

    set example added

--- basicCodeOfPython.py/1_function.py
+++ basicCodeOfPython.py/1_function.py
@@ -1,2 +1,8 @@
 def avg():
-    a = 0
+
+    a = int(input("enter the first number"))
+    b = int(input("enter the second number"))
+    c = int(input("enter the third number"))
+    average = (a+b+c)/3
+    print(f"the average of {a},{b},{c} is {average}")
+    avg(5)
