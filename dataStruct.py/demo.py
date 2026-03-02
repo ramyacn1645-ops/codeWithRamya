7752e37cc57c383859741ebd16c58eb2a1ad98b9
Ramya C N <ramyacn1645@gmail.com>
2026-03-02 23:26:10 +0530

    Add check for equilateral triangle in demo.py

--- dataStruct.py/demo.py
+++ dataStruct.py/demo.py
@@ -5,6 +5,7 @@ side_b =int(input("enter value of side_b:"))
 side_c =int(input("enter value of side_c:"))
 
 if(side_a==side_b and side_b==side_c):
+    
     print("it is an equilateral triangle")
 elif(side_a==side_b or side_b==side_c or side_a==side_c):
     print("it is an isosceles triangle")
