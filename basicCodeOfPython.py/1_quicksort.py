98f6cc48ee30b2866ae5be3e1123c956db62f3a1
Ramya C N <ramyacn1645@gmail.com>
2026-03-03 22:50:03 +0530

    Update 1_quicksort.py

--- basicCodeOfPython.py/1_quicksort.py
+++ basicCodeOfPython.py/1_quicksort.py
@@ -1,1 +1,8 @@
-
+def quicksort(cards):
+  if len(cards)==1:
+    return 1
+  else:
+    pivot = cards[0]
+    less = [i for i in cards if i <= pivot]
+    greater = [i for i  in cards if i > pivot ]
+    return quicksort[less] + pivot + quicksort[greater
