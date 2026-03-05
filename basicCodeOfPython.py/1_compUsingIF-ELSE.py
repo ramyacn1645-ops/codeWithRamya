be48e994b8fbe4328b26b62b4b839008770c9d90
Ramya C N <ramyacn1645@gmail.com>
2026-03-05 23:57:13 +0530

    Implement function to compare three numbers
    Added a function to compare three numbers and print the greatest.

--- basicCodeOfPython.py/1_compUsingIF-ELSE.py
+++ basicCodeOfPython.py/1_compUsingIF-ELSE.py
@@ -1,1 +1,12 @@
+def compare_three_nums(a,b,c):
+  if(a>b and a>c):
+    print(a,"is greater)
+  elif(b>c):
+    print(b,"is greater)
+  else:
+    print(c,"is greater)
 
+a = int(input("enter the value of a:"))
+b = int(input("enter the value of b:"))
+c = int(input("enter the value of c:"))
+print("the greatest of three number is ",compare_three_nums(a,b,c
