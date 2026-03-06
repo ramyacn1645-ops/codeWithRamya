74e9413af40f5ed659b87f6d887635dcd54a1c3c
Ramya C N <ramyacn1645@gmail.com>
2026-03-06 23:31:59 +0530

    leap/non leap

---
+++ basicCodeOfPython.py/2_problem2.py
@@ -0,0 +1,8 @@
+from datetime import date
+def findYear(year):
+    year = int(input("Enter a year: "))
+    if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+        print(year, "is a leap year.")
+    else:
+        print(year, "is not a leap year.")
+findYear(2027)
