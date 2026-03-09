231b0f92706d8e6dd2f78df9b8fd4cd3f3769e4d
Ramya C N <ramyacn1645@gmail.com>
2026-03-09 08:32:44 +0530

    Update 3_problem3.py

--- basicCodeOfPython.py/3_problem3.py
+++ basicCodeOfPython.py/3_problem3.py
@@ -15,4 +15,15 @@ for i in range(1, Num+1):
     add += i
     
 print(add,"is the total sum")
+
+
+# sum of 1st n natural numbers ising while loop
+Num = int(input("enter number upto which need sum:"))
+add = 0
+i =1
+while i <= Num
+    add += i
+    i += 1
+    
+print(add,"is the total sum")
     
