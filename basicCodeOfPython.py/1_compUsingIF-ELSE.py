b29ff36af629060ad9f157d9d8c61bddf22d8c6b
Ramya C N <ramyacn1645@gmail.com>
2026-03-21 21:51:39 +0530

    Update 1_compUsingIF-ELSE.py

--- basicCodeOfPython.py/1_compUsingIF-ELSE.py
+++ basicCodeOfPython.py/1_compUsingIF-ELSE.py
@@ -10,3 +10,15 @@ a = int(input("enter the value of a:"))
 b = int(input("enter the value of b:"))
 c = int(input("enter the value of c:"))
 print("the greatest of three number is ",compare_three_nums(a,b,c))  #function calling
+
+# Another method
+a = int(input("enter the value of a:"))
+b = int(input("enter the value of b:"))
+c = int(input("enter the value of c:"))
+
+if(a>b and a>c):
+    print(a,"is greater)
+  elif(b>c):
+    print(b,"is greater)
+  else:
+    print(c,"is greater)
