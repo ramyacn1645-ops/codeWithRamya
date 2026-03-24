81eb00b20e397f7cfc5fff0b57b1e1291bc68e91
Ramya C N <ramyacn1645@gmail.com>
2026-03-24 23:20:26 +0530

    Update 1_compUsingIF-ELSE.py

--- basicCodeOfPython.py/1_compUsingIF-ELSE.py
+++ basicCodeOfPython.py/1_compUsingIF-ELSE.py
@@ -22,3 +22,18 @@ if(a>b and a>c):
     print(b,"is greater)
   else:
     print(c,"is greater)
+
+# hike the salary based on their current salary 
+num = int(input("Enter the number of employee"))
+salary =[]
+print(f"Enter {num} employees salary")
+for i in range(num):
+    salary.append(float(input("Enter salary: ")))
+print(salary)
+for i in salary:
+    if salary>= 50000:
+        salary = salary + salary*0.2
+    else:
+        salary = salary + salary*0.3
+print(" updated salary is: ", salary
+    
