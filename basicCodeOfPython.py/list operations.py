8c416a8a4abb9966245fb3875d60b38dac168ea7
Ramya C N <ramyacn1645@gmail.com>
2026-03-26 23:46:07 +0530

    Update list operations.py

--- basicCodeOfPython.py/list operations.py
+++ basicCodeOfPython.py/list operations.py
@@ -14,3 +14,22 @@ print(list)
 list.pop(3) # removes the element in the index 3
 print(list)
 
+# copying the elements in the list to a another list
+list1 =[ 1,2,3,4,5]
+list2 = list1.copy() # it just copy the elements in the list
+
+id(list1)l
+print(list1)
+id(list2)
+# ID's are different for both the lists as the 2nd list only copy the elements of 1st list
+print(list2) 
+
+# copying the elements in the list to a another list using assignment operator
+list1 =[ 1,2,3,4,5]
+list2 = list1 # it copy the entire list1 i.e., it also points to the same memory location
+
+id(list1)l
+print(list1)
+id(list2)
+# ID's  of both the lists are same as the list2 copy tne list1 itself not just their elements 
+print(list2) 
