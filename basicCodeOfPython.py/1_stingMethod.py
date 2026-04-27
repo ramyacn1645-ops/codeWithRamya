504fa2d8423977b6702a92b314c2a104447901d3
Ramya C N <ramyacn1645@gmail.com>
2026-04-27 22:04:31 +0530

    updated

--- basicCodeOfPython.py/1_stingMethod.py
+++ basicCodeOfPython.py/1_stingMethod.py
@@ -1,2 +1,14 @@
 S1 = "  Welcome   "
 print(S1.strip())
+text = "python programming"
+
+print(text.upper())   # PYTHON PROGRAMMING
+print(text.lower())   # python programming
+print(text.title())   # Python Programming
+print(text.capitalize())  # Python programming
+
+text = "  hello  "
+
+print(text.strip())    # removes both sides spaces
+print(text.lstrip())   # left side
+print(text.rstrip())   # right side
